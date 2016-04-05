854862326891a7c30f0297db696d1c27329c1d9f
mariosky <mariosky@gmail.com>
2016-04-05 12:48:38 -0700

    Avance plot

--- data/plot-fit.R
+++ data/plot-fit.R
@@ -3,16 +3,18 @@ library(evd)
 library(fitdistrplus)
 
 
-suffix <- c("1","2","3")
+suffix <- c("1")
 for ( i in suffix ) {
-    data <- read.csv(paste0("data",i,".csv"))
-    ggplot(data,aes(x=as.numeric(row.names(data)),y=participation))+geom_point()+scale_y_log10()
-
+    data <- read.csv(paste0("/Users/mariosky/evoDrawing/data/data",i,".csv"))
+  # ggplot(data,aes(x=as.numeric(row.names(data)),y=participation)) +geom_point()+scale_y_log10()
+    
     this.fit <- fgev(data$participation)
-    print(paste0("Fit ",i, " GEV " ))
-    print(this.fit)
-
-    this.weibull <- fitdistr(data$participation,"weibull")
-    print(paste0("Fit ",i, " Weibull " ))
-    print(this.weibull)
+#    print(paste0("Fit ",i, " GEV " ))
+ #   print(this.fit)
+   
+    ggplot(data,aes(x=as.numeric(row.names(data)),y=participation))+geom_point()+scale_y_log10() +stat_function(fun = dgev, args = list(loc = 10.3478, scale = 10.1431, shape= 0.6411, log=TRUE))
+    
+    #this.weibull <- fitdistr(data$participation,"weibull")
+    #print(paste0("Fit ",i, " Weibull " ))
+    #print(this.weibull)
 }
