243891fa273ba653d6b1eef20e17f95f7b96d10c
JJ Merelo <jjmerelo@gmail.com>
2016-04-07 20:16:07 +0200

    Adapted paths
    But the fits do not look so good...

--- data/fitplot.R
+++ data/fitplot.R
@@ -19,11 +19,11 @@ ggplot(data,aes(x=as.numeric(row.names(data)),y=participation))+geom_point()+sca
 
 
 
-data <- read.csv(paste0("/Users/mariosky/evoDrawing/data/data",2,".csv"))
+data <- read.csv("data2.csv"))
 ggplot(data,aes(x=as.numeric(row.names(data)),y=participation))+geom_point()+scale_y_log10() +stat_function(fun = dgev, args = list(loc = 17.4977 , scale = 12.3651, shape= 0.3873 , log=TRUE))
 
 
-data <- read.csv(paste0("/Users/mariosky/evoDrawing/data/data",3,".csv"))
+data <- read.csv("data3.csv"))
 ggplot(data,aes(x=as.numeric(row.names(data)),y=participation))+geom_point()+scale_y_log10() +stat_function(fun = dgev, args = list(loc =22.327 , scale = 28.153, shape= 1.039  , log=TRUE))     
 
  
